56b89da39a823828d3805cd71acefe39ee073433
eikyuu <v.duguet.dev@gmail.com>
2026-04-15 15:39:21 +0200

    feat: remove unused imports from animes component for cleaner code

--- src/app/features/animes/animes.ts
+++ src/app/features/animes/animes.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { AnimeCard } from "./components/anime-card/anime-card";
-import { SlicePipe } from '@angular/common';
 import { AnimeList } from "./components/anime-list/anime-list";
 
 
 @Component({
   selector: 'app-animes',
-  imports: [AnimeCard, SlicePipe, AnimeList],
+  imports: [AnimeList],
   templateUrl: './animes.html',
   styleUrl: './animes.scss',
 })
